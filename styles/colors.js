34f5a9a0d70b1237aade31a299adfef236707b5b
samuel barasa <sammie@Sammies-MacBook-Pro.local>
2021-12-21 00:13:17 +0300

    - add styles and color variables

---
+++ styles/colors.js
@@ -0,0 +1,9 @@
+// colors
+export const COLOR_GREEN = "#06e670";
+export const COLOR_PINK = "#e81880";
+export const LIGHT_GRAY = "#f8f9ff";
+export const COLOR_FAUX_DARK = "#4a5d80";
+
+export const COLOR_BLACK = "#000000";
+export const COLOR_WHITE = "#ffffff";
+export const COLOR_TEXT = "#747474";
