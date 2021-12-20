270368569e337ac1d941326a59a4f65dab28cb87
samuel barasa <sammie@Sammies-MacBook-Pro.local>
2021-12-21 00:35:06 +0300

    - default layout

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
+import DefaultLayout from "./Layouts/DefaultLayout";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Home screen</Text>
-    </View>
+    <DefaultLayout>
+      <View style={styles.container}>
+        <Text>Home screen</Text>
+      </View>
+    </DefaultLayout>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    //flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
